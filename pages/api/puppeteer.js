8ce74ead5314866e864412126e98255908667c04
perplexedyawdie <sunkwenga@gmail.com>
2021-10-30 17:10:30 -0500

    updated config

--- pages/api/puppeteer.js
+++ pages/api/puppeteer.js
@@ -4,7 +4,7 @@ import puppeteer from 'puppeteer-core'
 export default async function handler(req, res) {
     // setup
     const browser = await puppeteer.launch({
-      executablePath: '../../node_modules/chromium/lib/chromium/chrome-linux/chrome',
+      executablePath: '/opt/build/repo/node_modules/chromium/lib/chromium/chrome-linux/chrome',
       headless: true,
     })
 
