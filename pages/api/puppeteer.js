cea638c2c70c316578beaa3d8fa47cc62ae7e1e9
perplexedyawdie <sunkwenga@gmail.com>
2021-10-30 01:33:29 -0500

    updated puppeteer

--- pages/api/puppeteer.js
+++ pages/api/puppeteer.js
@@ -1,11 +1,15 @@
 import chromium from 'chrome-aws-lambda';
+import puppeteer from 'puppeteer-core'
 
 export default async function handler(req, res) {
-  const browser = await chromium.puppeteer.launch({
-    executablePath: await chromium.executablePath,
-    args: chromium.args,
-    headless: chromium.headless
-  });
+    const executablePath = await chromium.executablePath
+
+    // setup
+    const browser = await puppeteer.launch({
+      args: chromium.args,
+      executablePath: executablePath,
+      headless: chromium.headless,
+    })
 
   res.status(200).json({ name: 'John Doe' })
 }
