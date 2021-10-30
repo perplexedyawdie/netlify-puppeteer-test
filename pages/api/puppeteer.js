c7e8a39faa5d3c5bb449e7f613776ae090e82900
perplexedyawdie <sunkwenga@gmail.com>
2021-10-30 18:33:02 -0500

    update

--- pages/api/puppeteer.js
+++ pages/api/puppeteer.js
@@ -3,26 +3,27 @@ import chromium from 'chromium'
 import path from 'path'
 const fs = require( 'fs' );
 const log = console.log;
-const folder = './opt';
+const folder = '/opt';
 export default async function handler(req, res) {
     // setup
-    console.log(process.env)
-    console.log(chromium.path)
-    console.log(__dirname)
-    // fs.readdirSync( folder ).forEach( file => {
+    // console.log(process.env)
+    // console.log(chromium.path)
+    // console.log(__dirname)
+    console.log(path.resolve(process.env.NODE_PATH, 'chromium', 'lib', 'chromium', 'chrome-linux', 'chrome-linux'))
+    fs.readdirSync( folder ).forEach( file => {
    
-    //     const extname = path.extname( file );
-    //     const filename = path.basename( file, extname );
-    //     const absolutePath = path.resolve( folder, file );
+        const extname = path.extname( file );
+        const filename = path.basename( file, extname );
+        const absolutePath = path.resolve( folder, file );
      
-    //     log( "File : ", file );
-    //     log( "filename : ", filename );
-    //     log( "extname : ", extname );
-    //     log( "absolutePath : ", absolutePath);
+        log( "File : ", file );
+        log( "filename : ", filename );
+        log( "extname : ", extname );
+        log( "absolutePath : ", absolutePath);
      
-    //  });
+     });
     const browser = await puppeteer.launch({
-      executablePath: '../../../node_modules/chromium/lib/chromium/chrome-linux/chrome',
+      executablePath: path.resolve(process.env.NODE_PATH, 'chromium', 'lib', 'chromium', 'chrome-linux', 'chrome-linux'),
       headless: true,
     })
 
