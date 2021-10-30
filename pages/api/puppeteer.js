cdb495a4b9a8a8913d4dc803235f076a67702b18
perplexedyawdie <sunkwenga@gmail.com>
2021-10-30 18:26:05 -0500

    update

--- pages/api/puppeteer.js
+++ pages/api/puppeteer.js
@@ -6,21 +6,21 @@ const log = console.log;
 const folder = './opt';
 export default async function handler(req, res) {
     // setup
-    console.log(process.env.CHROME_PATH)
+    console.log(process.env)
     console.log(chromium.path)
     console.log(__dirname)
-    fs.readdirSync( folder ).forEach( file => {
+    // fs.readdirSync( folder ).forEach( file => {
    
-        const extname = path.extname( file );
-        const filename = path.basename( file, extname );
-        const absolutePath = path.resolve( folder, file );
+    //     const extname = path.extname( file );
+    //     const filename = path.basename( file, extname );
+    //     const absolutePath = path.resolve( folder, file );
      
-        log( "File : ", file );
-        log( "filename : ", filename );
-        log( "extname : ", extname );
-        log( "absolutePath : ", absolutePath);
+    //     log( "File : ", file );
+    //     log( "filename : ", filename );
+    //     log( "extname : ", extname );
+    //     log( "absolutePath : ", absolutePath);
      
-     });
+    //  });
     const browser = await puppeteer.launch({
       executablePath: '../../../node_modules/chromium/lib/chromium/chrome-linux/chrome',
       headless: true,
