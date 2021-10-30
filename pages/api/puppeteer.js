ecfe20d118efd9cf88b2cec9cf2d384397f89979
perplexedyawdie <sunkwenga@gmail.com>
2021-10-30 17:00:02 -0500

    updated config

--- pages/api/puppeteer.js
+++ pages/api/puppeteer.js
@@ -1,10 +1,7 @@
-import chromium from 'chrome-aws-lambda';
 import puppeteer from 'puppeteer-core'
 
 
 export default async function handler(req, res) {
-    const executablePath = await chromium.executablePath
-    console.log(executablePath)
     // setup
     const browser = await puppeteer.launch({
       executablePath: '../../node_modules/chromium/lib/chromium/chrome-linux/chrome',
